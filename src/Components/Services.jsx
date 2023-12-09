d312d584b16ebd2f2660e4c352902f1b7f9ea4f4
Sarthakx27 <sarrthakbhatt327@gmail.com>
2023-12-09 15:11:21 +0530

    Changes

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -15,7 +15,7 @@ const Services = () => {
         </h1>
       </div>
       <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 lg:grid-rows-2 gap-3 md:gap-16 lg:gap-16 mt-8 md:mt-20 lg:mt-20">
-        <div className="group flex flex-col border-2 rounded-3xl justify-center items-center px-5 py-7 md:px-16 md:py-10 lg:px-16 lg:py-14 hover:bg-violet-400 duration-500">
+        <div className="group flex flex-col border-2 rounded-3xl justify-center items-center px-2 py-5 md:px-16 md:py-10 lg:px-16 lg:py-14 hover:bg-violet-400 duration-500">
           <div className=" bg-violet-100 rounded-2xl relative h-16 w-16 md:w-28 md:h-28 lg:h-28 lg:w-28 flex justify-center items-center">
             {/* <img className=' w-10 h-10 md:w-16 md:h-16 lg:w-16 lg:h-16' src="https://websitedemos.net/astra-lite-demo-1/wp-content/uploads/sites/30/2017/06/editing.png" alt="" /> */}
             <ArchitectureIcon style={{ color: "#cca3ff", fontSize: "4rem"}} />
@@ -28,7 +28,7 @@ const Services = () => {
             acclimatize instantly and beneficially.
           </p>
         </div>
-        <div className="group flex flex-col border-2 rounded-3xl justify-center items-center px-5 py-7 md:px-16 md:py-10 lg:px-16 lg:py-14 hover:bg-green-400 duration-500">
+        <div className="group flex flex-col border-2 rounded-3xl justify-center items-center px-2 py-5 md:px-16 md:py-10 lg:px-16 lg:py-14 hover:bg-green-400 duration-500">
         <div className=" bg-green-100 rounded-2xl relative h-16 w-16 md:w-28 md:h-28 lg:h-28 lg:w-28 flex justify-center items-center">
             {/* <img
               className="w-10 h-10 md:w-16 md:h-16 lg:w-16 lg:h-16"
@@ -48,7 +48,7 @@ const Services = () => {
           </p>
         </div>
 
-        <div className="group flex flex-col border-2 rounded-3xl justify-center items-center px-5 py-7 md:px-16 md:py-10 lg:px-16 lg:py-14 hover:bg-cyan-400 duration-500">
+        <div className="group flex flex-col border-2 rounded-3xl justify-center items-center px-2 py-5 md:px-16 md:py-10 lg:px-16 lg:py-14 hover:bg-cyan-400 duration-500">
           <div className=" bg-cyan-100 rounded-2xl relative h-16 w-16 md:w-28 md:h-28 lg:h-28 lg:w-28 flex justify-center items-center">
             {/* <img
               className=" w-10 h-10 md:w-16 md:h-16 lg:w-16 lg:h-16"
@@ -67,7 +67,7 @@ const Services = () => {
             promote products and services.
           </p>
         </div>
-        <div className="group flex flex-col border-2 rounded-3xl justify-center items-center px-5 py-7 md:px-16 md:py-10 lg:px-16 lg:py-14 hover:bg-orange-400 duration-500">
+        <div className="group flex flex-col border-2 rounded-3xl justify-center items-center px-2 py-5 md:px-16 md:py-10 lg:px-16 lg:py-14 hover:bg-orange-400 duration-500">
           <div className=" bg-orange-100 rounded-2xl relative h-16 w-16 md:w-28 md:h-28 lg:h-28 lg:w-28 flex justify-center items-center">
             {/* <img
               className=" w-10 h-10 md:w-16 md:h-16 lg:w-16 lg:h-16"
@@ -77,7 +77,7 @@ const Services = () => {
             <TimelineIcon style={{ color: "#FF7518", fontSize: "4rem"}}/>
           </div>
           <h1 className="text-orange-400 py-4 md:py-7 lg:py-7 font-bold text-base md:text-xl lg:text-xl group-hover:text-white duration-500">
-            ENTERPRISE MOBILITY
+            Enterprise Mobility
           </h1>
           <p className="text-gray-600 text-xs md:text-base lg:text-base group-hover:text-white duration-500">
             Our far-reaching span of assistance is designed to provide
@@ -85,7 +85,7 @@ const Services = () => {
             aid these products.
           </p>
         </div>
-        <div className="group flex flex-col border-2 rounded-3xl justify-center items-center px-5 py-7 md:px-16 md:py-10 lg:px-16 lg:py-14 hover:bg-blue-500 duration-500">
+        <div className="group flex flex-col border-2 rounded-3xl justify-center items-center px-2 py-5 md:px-16 md:py-10 lg:px-16 lg:py-14 hover:bg-blue-500 duration-500">
           <div className=" bg-blue-100 rounded-2xl relative h-16 w-16 md:w-28 md:h-28 lg:h-28 lg:w-28 flex justify-center items-center">
             {/* <img
               className=" w-10 h-10 md:w-16 md:h-16 lg:w-16 lg:h-16"
@@ -95,14 +95,14 @@ const Services = () => {
             <SupportAgentTwoToneIcon style={{ color: "#4169E1", fontSize: "4rem"}}/>
           </div>
           <h1 className="text-blue-500 py-4 md:py-7 lg:py-7 font-bold text-base md:text-xl lg:text-xl group-hover:text-white duration-500">
-            CONSULTANCY
+            Consultancy
           </h1>
           <p className="text-gray-600 text-xs md:text-base lg:text-base group-hover:text-white duration-500">
             We look into Customized Solutions, IT review & Audit, Information &
             Data management, Software development and maintenance, etc.
           </p>
         </div>
-        <div className="group flex flex-col border-2 rounded-3xl justify-center items-center px-5 py-7 md:px-16 md:py-10 lg:px-16 lg:py-14 hover:bg-gray-500 duration-500">
+        <div className="group flex flex-col border-2 rounded-3xl justify-center items-center px-2 py-5 md:px-16 md:py-10 lg:px-16 lg:py-14 hover:bg-gray-500 duration-500">
         <div className=" bg-gray-100 rounded-2xl relative h-16 w-16 md:w-28 md:h-28 lg:h-28 lg:w-28 flex justify-center items-center">
             {/* <img
               className=" w-10 h-10 md:w-16 md:h-16 lg:w-16 lg:h-16"
