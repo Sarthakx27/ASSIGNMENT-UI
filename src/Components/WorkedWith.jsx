f94f226584eedd07381b2f715189b635fc876e3e
Sarthakx27 <sarrthakbhatt327@gmail.com>
2023-12-09 13:25:41 +0530

    Changes

--- src/Components/WorkedWith.jsx
+++ src/Components/WorkedWith.jsx
@@ -1,106 +1,179 @@
-import React from 'react'
+import React from "react";
 
 const WorkedWith = () => {
   return (
-    <div className='font-display w-11/12 mx-auto flex flex-col justify-center text-center items-center pt-9 md:pt-20 lg:pt-20 mb-10'>
+    <div className="font-display w-11/12 mx-auto flex flex-col justify-center text-center items-center pt-9 md:pt-20 lg:pt-20 mb-10">
       <div>
-      <h1 className=' font-bold md:text-3xl lg:text-5xl text-gray-800'>Who we've worked with</h1>
+        <h1 className=" font-bold md:text-3xl lg:text-5xl text-gray-800">
+          Who we've worked with
+        </h1>
       </div>
-      <div className='grid grid-cols-3 lg:grid-cols-6 gap-5 lg:gap-16 mt-8 md:mt-16 lg:mt-20'>
-        <div className='flex flex-col border-2 rounded-3xl justify-center items-center px-5 py-5 hover:bg-slate-100 duration-500'>
-          <div className='flex justify-center items-center'>
-          <img className=' w-full'  src="https://www.cantechnologies.co.in/wp-content/uploads/elementor/thumbs/1-1-owzzrwp8jlhb3x0hvtbg75xxkbw3qa5gvnbcfj83u6.png" alt="" />
+      <div className="grid grid-cols-3 lg:grid-cols-6 gap-5 lg:gap-16 mt-8 md:mt-16 lg:mt-20">
+        <div className="flex flex-col border-2 rounded-3xl justify-center items-center px-5 py-5 hover:bg-slate-100 duration-500">
+          <div className="flex justify-center items-center">
+            <img
+              className=" w-full rounded-2xl"
+              src="https://www.cantechnologies.co.in/wp-content/uploads/elementor/thumbs/1-1-owzzrwp8jlhb3x0hvtbg75xxkbw3qa5gvnbcfj83u6.png"
+              alt=""
+            />
           </div>
         </div>
-        <div className='flex flex-col border-2 rounded-3xl justify-center items-center px-5 py-5 hover:bg-slate-100 duration-500'>
-          <div className='flex justify-center items-center'>
-          <img className=' w-full'  src="https://www.cantechnologies.co.in/wp-content/uploads/elementor/thumbs/2-1-owzzs55s93sw0eo7iez3blt2wsqenk31wt6pr0vka6.png" alt="" />
+        <div className="flex flex-col border-2 rounded-3xl justify-center items-center px-5 py-5 hover:bg-slate-100 duration-500">
+          <div className="flex justify-center items-center">
+            <img
+              className=" w-full rounded-2xl"
+              src="https://www.cantechnologies.co.in/wp-content/uploads/elementor/thumbs/2-1-owzzs55s93sw0eo7iez3blt2wsqenk31wt6pr0vka6.png"
+              alt=""
+            />
           </div>
         </div>
-        <div className='flex flex-col border-2 rounded-3xl justify-center items-center px-5 py-5 hover:bg-slate-100 duration-500'>
-          <div className='flex justify-center items-center'>
-          <img className=' w-full'  src="https://www.cantechnologies.co.in/wp-content/uploads/elementor/thumbs/3-1-owzzs55s93sw0eo7iez3blt2wsqenk31wt6pr0vka6.png" alt="" />
+        <div className="flex flex-col border-2 rounded-3xl justify-center items-center px-5 py-5 hover:bg-slate-100 duration-500">
+          <div className="flex justify-center items-center">
+            <img
+              className=" w-full rounded-2xl"
+              src="https://www.cantechnologies.co.in/wp-content/uploads/elementor/thumbs/3-1-owzzs55s93sw0eo7iez3blt2wsqenk31wt6pr0vka6.png"
+              alt=""
+            />
           </div>
         </div>
-        <div className='flex flex-col border-2 rounded-3xl justify-center items-center px-5 py-5 hover:bg-slate-100 duration-500'>
-          <div className='flex justify-center items-center'>
-          <img className=' w-full'  src="https://www.cantechnologies.co.in/wp-content/uploads/elementor/thumbs/4-1-oxasgdpxuc4d085zo4yho7og3orvsvarbyh59gg2ge.png" alt="" />
+        <div className="flex flex-col border-2 rounded-3xl justify-center items-center px-5 py-5 hover:bg-slate-100 duration-500">
+          <div className="flex justify-center items-center">
+            <img
+              className=" w-full rounded-2xl"
+              src="https://www.cantechnologies.co.in/wp-content/uploads/elementor/thumbs/4-1-oxasgdpxuc4d085zo4yho7og3orvsvarbyh59gg2ge.png"
+              alt=""
+            />
           </div>
         </div>
-        <div className='flex flex-col border-2 rounded-3xl justify-center items-center px-5 py-5 hover:bg-slate-100 duration-500'>
-          <div className='flex justify-center items-center'>
-          <img className=' w-full'  src="https://www.cantechnologies.co.in/wp-content/uploads/elementor/thumbs/5-1-oxasg82wpbwn2ke6l2iq993ojdjoioodb6k8dsofhq.png" alt="" />
+        <div className="flex flex-col border-2 rounded-3xl justify-center items-center px-5 py-5 hover:bg-slate-100 duration-500">
+          <div className="flex justify-center items-center">
+            <img
+              className=" w-full rounded-2xl"
+              src="https://www.cantechnologies.co.in/wp-content/uploads/elementor/thumbs/5-1-oxasg82wpbwn2ke6l2iq993ojdjoioodb6k8dsofhq.png"
+              alt=""
+            />
           </div>
         </div>
-        <div className='flex flex-col border-2 rounded-3xl justify-center items-center px-5 py-5 hover:bg-slate-100 duration-500'>
-          <div className='flex justify-center items-center'>
-          <img className=' w-full'  src="https://www.cantechnologies.co.in/wp-content/uploads/elementor/thumbs/6-1-owzzs3a3vfqbd6qxte5u6ma5q0zo85vl8jvqsgycmm.png" alt="" />
+        <div className="flex flex-col border-2 rounded-3xl justify-center items-center px-5 py-5 hover:bg-slate-100 duration-500">
+          <div className="flex justify-center items-center">
+            <img
+              className=" w-full rounded-2xl"
+              src="https://www.cantechnologies.co.in/wp-content/uploads/elementor/thumbs/6-1-owzzs3a3vfqbd6qxte5u6ma5q0zo85vl8jvqsgycmm.png"
+              alt=""
+            />
           </div>
         </div>
-        <div className='flex flex-col border-2 rounded-3xl justify-center items-center px-5 py-5 hover:bg-slate-100 duration-500'>
-          <div className='flex justify-center items-center'>
-          <img className=' w-full'  src="https://www.cantechnologies.co.in/wp-content/uploads/elementor/thumbs/8-1-owzzs2c9olp11ksayvr7m4ip4n4b0gruwf89b6zqsu.png" alt="" />
+        <div className="flex flex-col border-2 rounded-3xl justify-center items-center px-5 py-5 hover:bg-slate-100 duration-500">
+          <div className="flex justify-center items-center">
+            <img
+              className=" w-full rounded-2xl"
+              src="https://www.cantechnologies.co.in/wp-content/uploads/elementor/thumbs/8-1-owzzs2c9olp11ksayvr7m4ip4n4b0gruwf89b6zqsu.png"
+              alt=""
+            />
           </div>
         </div>
-        <div className='flex flex-col border-2 rounded-3xl justify-center items-center px-5 py-5 hover:bg-slate-100 duration-500'>
-          <div className='flex justify-center items-center'>
-          <img className=' w-full'  src="https://www.cantechnologies.co.in/wp-content/uploads/elementor/thumbs/9-1-owzzs2c9olp11ksayvr7m4ip4n4b0gruwf89b6zqsu.jpg" alt="" />
+        <div className="flex flex-col border-2 rounded-3xl justify-center items-center px-5 py-5 hover:bg-slate-100 duration-500">
+          <div className="flex justify-center items-center">
+            <img
+              className=" w-full rounded-2xl"
+              src="https://www.cantechnologies.co.in/wp-content/uploads/elementor/thumbs/9-1-owzzs2c9olp11ksayvr7m4ip4n4b0gruwf89b6zqsu.jpg"
+              alt=""
+            />
           </div>
         </div>
-        <div className='flex flex-col border-2 rounded-3xl justify-center items-center px-5 py-5 hover:bg-slate-100 duration-500'>
-          <div className='flex justify-center items-center'>
-          <img className=' w-full'  src="https://www.cantechnologies.co.in/wp-content/uploads/elementor/thumbs/10-1-owzzs1efhrnqpyto4dcl1mr8j98xsro4kakrtx14z2.png" alt="" />
+        <div className="flex flex-col border-2 rounded-3xl justify-center items-center px-5 py-5 hover:bg-slate-100 duration-500">
+          <div className="flex justify-center items-center">
+            <img
+              className=" w-full rounded-2xl"
+              src="https://www.cantechnologies.co.in/wp-content/uploads/elementor/thumbs/10-1-owzzs1efhrnqpyto4dcl1mr8j98xsro4kakrtx14z2.png"
+              alt=""
+            />
           </div>
         </div>
-        <div className='flex flex-col border-2 rounded-3xl justify-center items-center px-5 py-5 hover:bg-slate-100 duration-500'>
-          <div className='flex justify-center items-center'>
-          <img className=' w-full'  src="https://www.cantechnologies.co.in/wp-content/uploads/elementor/thumbs/11-1-owzzs0glaxmgecv19uxyh4zrxvdkl2ke85xacn2j5a.png" alt="" />
+        <div className="flex flex-col border-2 rounded-3xl justify-center items-center px-5 py-5 hover:bg-slate-100 duration-500">
+          <div className="flex justify-center items-center">
+            <img
+              className=" w-full rounded-2xl"
+              src="https://www.cantechnologies.co.in/wp-content/uploads/elementor/thumbs/11-1-owzzs0glaxmgecv19uxyh4zrxvdkl2ke85xacn2j5a.png"
+              alt=""
+            />
           </div>
         </div>
-        <div className='flex flex-col border-2 rounded-3xl justify-center items-center px-5 py-5 hover:bg-slate-100 duration-500'>
-          <div className='flex justify-center items-center'>
-          <img className=' w-full'  src="https://www.cantechnologies.co.in/wp-content/uploads/elementor/thumbs/13-1-oxasggjgeu87z21w7o6ddoytvudzfylyccflpabvxq.png" alt="" />
+        <div className="flex flex-col border-2 rounded-3xl justify-center items-center px-5 py-5 hover:bg-slate-100 duration-500">
+          <div className="flex justify-center items-center">
+            <img
+              className=" w-full rounded-2xl"
+              src="https://www.cantechnologies.co.in/wp-content/uploads/elementor/thumbs/13-1-oxasggjgeu87z21w7o6ddoytvudzfylyccflpabvxq.png"
+              alt=""
+            />
           </div>
         </div>
-        <div className='flex flex-col border-2 rounded-3xl justify-center items-center px-5 py-5 hover:bg-slate-100 duration-500'>
-          <div className='flex justify-center items-center'>
-          <img className=' w-full'  src="https://www.cantechnologies.co.in/wp-content/uploads/elementor/thumbs/14-1-oxasgawf9u0i1ea34lqlyqe2bj5s5rzkbkiotmk8z2.png" alt="" />
+        <div className="flex flex-col border-2 rounded-3xl justify-center items-center px-5 py-5 hover:bg-slate-100 duration-500">
+          <div className="flex justify-center items-center">
+            <img
+              className=" w-full rounded-2xl"
+              src="https://www.cantechnologies.co.in/wp-content/uploads/elementor/thumbs/14-1-oxasgawf9u0i1ea34lqlyqe2bj5s5rzkbkiotmk8z2.png"
+              alt=""
+            />
           </div>
         </div>
-        <div className='flex flex-col border-2 rounded-3xl justify-center items-center px-5 py-5 hover:bg-slate-100 duration-500'>
-          <div className='flex justify-center items-center'>
-          <img className=' w-full'  src="https://www.cantechnologies.co.in/wp-content/uploads/elementor/thumbs/15-1-owzzrykwx9jvr4xrku4pc5gur3mu5ocxjwmbe35bhq.png" alt="" />
+        <div className="flex flex-col border-2 rounded-3xl justify-center items-center px-5 py-5 hover:bg-slate-100 duration-500">
+          <div className="flex justify-center items-center">
+            <img
+              className=" w-full rounded-2xl"
+              src="https://www.cantechnologies.co.in/wp-content/uploads/elementor/thumbs/15-1-owzzrykwx9jvr4xrku4pc5gur3mu5ocxjwmbe35bhq.png"
+              alt=""
+            />
           </div>
         </div>
-        <div className='flex flex-col border-2 rounded-3xl justify-center items-center px-5 py-5 hover:bg-slate-100 duration-500'>
-          <div className='flex justify-center items-center'>
-          <img className=' w-full'  src="https://www.cantechnologies.co.in/wp-content/uploads/elementor/thumbs/16-2-oxasg678bnu2fcgww1ph49krclsy3agwmx99f8r7u6.png" alt="" />
+        <div className="flex flex-col border-2 rounded-3xl justify-center items-center px-5 py-5 hover:bg-slate-100 duration-500">
+          <div className="flex justify-center items-center">
+            <img
+              className=" w-full rounded-2xl"
+              src="https://www.cantechnologies.co.in/wp-content/uploads/elementor/thumbs/16-2-oxasg678bnu2fcgww1ph49krclsy3agwmx99f8r7u6.png"
+              alt=""
+            />
           </div>
         </div>
-        <div className='flex flex-col border-2 rounded-3xl justify-center items-center px-5 py-5 hover:bg-slate-100 duration-500'>
-          <div className='flex justify-center items-center'>
-          <img className=' w-full'  src="https://www.cantechnologies.co.in/wp-content/uploads/elementor/thumbs/17-1-owzzrxn2qfilfiz4qbq2rnpe5prgxz977rytwt6pny.png" alt="" />
+        <div className="flex flex-col border-2 rounded-3xl justify-center items-center px-5 py-5 hover:bg-slate-100 duration-500">
+          <div className="flex justify-center items-center">
+            <img
+              className=" w-full rounded-2xl"
+              src="https://www.cantechnologies.co.in/wp-content/uploads/elementor/thumbs/17-1-owzzrxn2qfilfiz4qbq2rnpe5prgxz977rytwt6pny.png"
+              alt=""
+            />
           </div>
         </div>
-        <div className='flex flex-col border-2 rounded-3xl justify-center items-center px-5 py-5 hover:bg-slate-100 duration-500'>
-          <div className='flex justify-center items-center'>
-          <img className=' w-full'  src="https://www.cantechnologies.co.in/wp-content/uploads/elementor/thumbs/18-1-owzzrvrecrg0sb1v1awtmo6gyy0qil1qjinuy99htq.png" alt="" />
+        <div className="flex flex-col border-2 rounded-3xl justify-center items-center px-5 py-5 hover:bg-slate-100 duration-500">
+          <div className="flex justify-center items-center">
+            <img
+              className=" w-full rounded-2xl"
+              src="https://www.cantechnologies.co.in/wp-content/uploads/elementor/thumbs/18-1-owzzrvrecrg0sb1v1awtmo6gyy0qil1qjinuy99htq.png"
+              alt=""
+            />
           </div>
         </div>
-        <div className='flex flex-col border-2 rounded-3xl justify-center items-center px-5 py-5 hover:bg-slate-100 duration-500'>
-          <div className='flex justify-center items-center'>
-          <img className=' w-full'  src="https://www.cantechnologies.co.in/wp-content/uploads/elementor/thumbs/12-1-owzzs0glaxmgecv19uxyh4zrxvdkl2ke85xacn2j5a.png" alt="" />
+        <div className="flex flex-col border-2 rounded-3xl justify-center items-center px-5 py-5 hover:bg-slate-100 duration-500">
+          <div className="flex justify-center items-center">
+            <img
+              className=" w-full rounded-2xl"
+              src="https://www.cantechnologies.co.in/wp-content/uploads/elementor/thumbs/12-1-owzzs0glaxmgecv19uxyh4zrxvdkl2ke85xacn2j5a.png"
+              alt=""
+            />
           </div>
         </div>
-        <div className='flex flex-col border-2 rounded-3xl justify-center items-center px-5 py-5 hover:bg-slate-100 duration-500'>
-          <div className='flex justify-center items-center'>
-          <img className=' w-full'  src="https://www.cantechnologies.co.in/wp-content/uploads/elementor/thumbs/7-1-owzzs3a3vfqbd6qxte5u6ma5q0zo85vl8jvqsgycmm.png" alt="" />
+        <div className="flex flex-col border-2 rounded-3xl justify-center items-center px-5 py-5 hover:bg-slate-100 duration-500">
+          <div className="flex justify-center items-center">
+            <img
+              className=" w-full rounded-2xl"
+              src="https://www.cantechnologies.co.in/wp-content/uploads/elementor/thumbs/7-1-owzzs3a3vfqbd6qxte5u6ma5q0zo85vl8jvqsgycmm.png"
+              alt=""
+            />
           </div>
         </div>
-        
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default WorkedWith
+export default WorkedWith;
